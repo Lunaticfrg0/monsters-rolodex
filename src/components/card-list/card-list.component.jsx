df1023e3268a2d7a3b3c22297f09e9928cd02e23
Lunaticfrg0 <emilio3scano@gmail.com>
2022-10-26 17:30:45 -0400

    converted components to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,19 @@
-import { Component } from "react";
 import  "./card-list.styles.css";
 import Card from "../card/card.component";
 
-class CardList extends Component {
-
-    render(){
-        const {monsters} = this.props
-        return (
-            <div className="card-list"> 
-                {monsters.map((monster) => {
-                    
-                    return (
-                          <Card monster = {monster}/>
-                    )
-                }
-                )}
-            </div>
-        )
-    }
+//Implicit decinstruct bc its the only prop
+const CardList = ({monsters}) =>  {
+    return (
+        <div className="card-list"> 
+            {monsters.map((monster) => {
+                
+                return (
+                      <Card monster = {monster}/>
+                )
+            }
+            )}
+        </div>
+    )
 }
 
 export default CardList;
