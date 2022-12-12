8e37617ca6b5e2516ccd08e04a4189f29273e9b9
Lunaticfrg0 <emilio3scano@gmail.com>
2022-12-12 18:16:23 -0400

    moved rolodex to typescript

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,8 +1,12 @@
 import  "./card-list.styles.css";
 import Card from "../card/card.component";
-
+import { Monster } from "../../App";
+ 
+type CardListProps = {
+    monsters: Monster[]
+}
 //Implicit decinstruct bc its the only prop
-const CardList = ({monsters}) =>  {
+const CardList = ({monsters}: CardListProps) =>  {
     return (
         <div className="card-list"> 
             {monsters.map((monster) => {
